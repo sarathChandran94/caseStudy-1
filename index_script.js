b218c02fa71ce45e8a9a3dd055e5a7de0f00aaa6
Sarath C <sarathcs3c@gmail.com>
2020-06-08 16:53:30 +0530

    added styling to index.html and added promise to mainp_script.js

--- index_script.js
+++ index_script.js
@@ -7,8 +7,8 @@
         myform.attr('action','main_page.html');
         myform.attr('onsubmit','return validate()');
         callback();
-        form[0].style.border = "";
-        form[1].style.border = "";
+        // form[0].style.border = "";
+        // form[1].style.border = "";
     }
 
     var validate = () => {
@@ -16,13 +16,13 @@
             return true;
         }
         if(form[0].value == "" || form[1].value == "") {
-            form[0].style.border = "1px solid red";
-            form[1].style.border = "1px solid red";
+            // form[0].style.border = "1px solid red";
+            // form[1].style.border = "1px solid red";
             return false;
         }
         else {
-            form[0].style.border = "2px solid blue";
-            form[1].style.border = "2px solid blue";
+            // form[0].style.border = "2px solid blue";
+            // form[1].style.border = "2px solid blue";
             return false;
         }
     }
--- mainp_script.js
+++ mainp_script.js
@@ -1,73 +1,61 @@
 // main_page_javascript
+
 $(document).ready(function() {
-    var items = [],toDisp =[],disp = [],objVal = [],list =[],list1 = [],list2 =[],output = [];
-    var i,j;
-    $(document).load('https://jsonplaceholder.typicode.com/todos',function(res,stat,xhr) {
+
+    // variable declarations
+    var items = [],list = [],list1 = [],clicks = 0;
+
+    // Accessing Todo List
+    let getList = () => {$(document).load('https://jsonplaceholder.typicode.com/todos',function(res,stat,xhr) {
+
+        // page loading animation
+        $("#loader").hide().ajaxStart( () => {$(this).show();}).ajaxStop( () => {$(this).hide();});
+
         items = JSON.parse(res);
-        // document.getElementById('display').innerHTML = items;
-        for (var x=0;x<items.length;x++) {
-            output += items[x];
-        }
-        // console.log(output);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        $.each(items,function(key,value) {
-            // console.log(key);
-            console.log(value.completed);
-            list += `<tr>
-            <td style="border-bottom: 1px solid red;">${value.id}</td>
-            <td style="border-bottom: 1px solid red;">${value.title}</td>
-            <td style="border-bottom: 1px solid red;">${value.completed}</td>
-            <td style="border-bottom: 1px solid red;"><label><input type="checkbox"/><span></span></label></td>
-            </tr>`;
-            $("#list").html(`${list}`);
-            if (value.completed) {
-                $("input").attr("checked","checked");
-            }
+        // promise
+        var promise = new Promise( (resolve,reject) => {
+            if(stat == "success") {resolve();}
+            if(stat == "failure") {reject(`Sorry Error code: ${xhr.status}`)}
         });
 
+        // promise resolution
+        promise
+        .then( (s) => {getTable();})
+        .then( () => {taskCheck();})
+        .catch( (e) => {console.log(e)});
+    })};
 
+    // Creating Table elements
+    let getTable = () => {$.each(items,function(key,value) {
+        if (value.completed) {
+            list1 = `<td style="border-bottom: 1px solid red;"><label><input type="checkbox" checked disabled/><span></span></label></td>`;
+        }
+        if(!value.completed) {
+            list1 = `<td style="border-bottom: 1px solid red;"><label><input type="checkbox"/><span></span></label></td>`;
+        }
+        list += `<tr>
+                    <td style="border-bottom: 1px solid red;">${value.id}</td>
+                    <td style="border-bottom: 1px solid red;">${value.title}</td>
+                    <td style="border-bottom: 1px solid red;">${value.completed}</td>
+                    ${list1}
+                </tr>`;
+        $("#list").html(`${list}`);
+    })};
 
+    // checking for task completions
+    let taskCheck = () => {$("input").on("change", () => {
+        event.preventDefault();
+        $("input:focus").prop("disabled","disabled");
+        ++clicks;
+        if (clicks == 5) {
+            Swal.fire(
+                'Good job!',
+                'You\'ve completed 5 tasks.',
+                'success'
+            );
+        }
+    })};
 
-
-
-
-
-
-
-
-
-
-
-
-
-        // output1.forEach(element => {
-        //     if (element == "true") {
-        //         console.log(element);
-        //         return true;
-        //         // trueItem += element;
-        //     }
-        //     // if(element == "false") {
-        //     //     falseItem += element;
-        //     //     console.log(element);
-        //     // }
-        // });
-    });
+    getList();
 });
